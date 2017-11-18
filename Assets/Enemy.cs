9c64fc446648209ecea8b1aa0a6c7bee116a1901
ZetaDesigns <arzisus@gmail.com>
2017-11-18 15:23:17 +0100

    Waypoint system, first map

---
+++ Assets/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    void Start ()
+    {
+        target = Waypoints.waypoints[0];
+    }
+    void Update ()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.waypoints.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        waypointIndex++;
+        target = Waypoints.waypoints[waypointIndex];
+    }
+}
