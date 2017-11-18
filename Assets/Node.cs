3c53c030f6565dcdacd7965b8c151f9e9c47eff1
ZetaDesigns <arzisus@gmail.com>
2017-11-18 20:22:07 +0100

    Impact and MouseHover

---
+++ Assets/Node.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color oldColor;
+    void Start ()
+    {
+        rend = GetComponent<Renderer>();
+        oldColor = rend.material.color;
+    }
+    void OnMouseDown ()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Already built something here");
+        }
+
+        //Build a turret
+
+    }
+	void OnMouseEnter ()
+    {
+       rend.material.color = hoverColor;
+    }
+    void OnMouseExit ()
+    {
+        rend.material.color = oldColor;
+    }
+}
