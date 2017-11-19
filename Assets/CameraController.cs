d31f6daf3b1034e47b47ea44526ff0e5b4cf2bcf
ZetaDesigns <arzisus@gmail.com>
2017-11-19 16:10:28 +0100

    Camera movement yeay

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,16 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    public float panSpeed = 30f;
+    public float BorderThickness = 20f;
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - BorderThickness)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= BorderThickness)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - BorderThickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= BorderThickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+    }
 }
