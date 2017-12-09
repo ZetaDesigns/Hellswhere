a83efa4b1754d96905cf9104481cfaae3bf21d79
ZetaDesigns <arzisus@gmail.com>
2017-12-09 23:30:16 +0100

    wave counter

--- Assets/Scripts/MoneyUI.cs
+++ Assets/Scripts/MoneyUI.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 public class MoneyUI : MonoBehaviour {
 
     public Text moneyText;
+    public Text wavesText;
 	// Update is called once per frame
 	void Update () {
         moneyText.text = "$" + StatsManager.Money.ToString();
-	}
+        wavesText.text = "Waves Survived: " + StatsManager.Waves.ToString();
+}
 }
